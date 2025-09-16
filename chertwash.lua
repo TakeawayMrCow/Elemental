20d30a249ae39b91a1fb7612a499dadf100e6a3f
Takeaway Mr Cow <tomh@eutony.net>
2025-09-16 19:22:39 +0100

    churt more

---
+++ chertwash.lua
@@ -0,0 +1,34 @@
+chertwashRecipe = {
+    type = "recipe",
+    name = "chertwash",
+    
+    category = "chemistry", 
+    subgroup = "elemental-basechem",
+    
+
+    energy_required = 5, -- How long to make
+    enabled = true,
+
+    ingredients = {
+      { type = "item", name = "chert", amount = 6},
+      { type = "fluid", name = "water", amount = 60},
+    },
+    results = {
+      { type = "item", name = "washedchert", amount =5, probability = 0.9},
+      { type = "item", name = "clay", amount =3, probability = 0.15},
+    },
+
+    allow_decomposition = false,
+
+    icon = "__elemental__/graphics/chertwash.png",
+    icon_mipmaps = 4,
+    icon_size = 64,
+    crafting_machine_tint = {
+      primary = {r = 0.15, g = 0.16, b = 0.4},
+      secondary = {r = 0.15, g = 0.16, b = 0.4},
+      teriary = {r = 0.15, g = 0.16, b = 0.4},
+      quaternary = {r = 0.1, g = 0.2, b = 0.5},
+    },  --  primary = fluid, secondary = foam, tertiary = smoke outer, quaternary = smoke inner
+}
+
+data:extend{chertwashRecipe}
---
+++ washedchert.lua
@@ -0,0 +1,12 @@
+local washedchertItem = {
+  name = "washedchert",
+  type = "item",
+  icon = "__elemental__/graphics/chertwash.png",
+  -- order = "c",
+  fuel_category = "chemical",
+  fuel_value = "1.5MJ",
+  stack_size = 20,
+  subgroup = "elemental-baseprod"
+}
+
+data:extend{washedchertItem}
