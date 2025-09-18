a610c3576c8f04219e3ba91ed2c79b1aa3c7e07d
TakeawayMrCow <tomh@eutony.net>
2025-09-18 16:21:34 +0100

    clay ahh

---
+++ clay.lua
@@ -0,0 +1,10 @@
+local clayItem = {
+  name = "clay",
+  type = "item",
+  icon = "__elemental__/graphics/clay.png",
+  -- order = "c",
+  stack_size = 25,
+  subgroup = "elemental-baseprod"
+}
+
+data:extend{clayItem}
