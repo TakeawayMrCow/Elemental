1d3457a84a6544ff3865577f7c138db8280cc348
TakeawayMrCow <tomh@eutony.net>
2026-04-03 12:08:56 +0100

    PGM fix

--- items/oxidisedPt.lua
+++ items/oxidisedPt.lua
@@ -1,7 +1,7 @@
 local oxidisedPtItem = {
   name = "oxidisedPt",
   type = "item",
-  icon = "__elemental__/graphics/oxidisedPt.png",
+  icon = "__elemental__/graphics/PGM/oxidisedPt.png",
   -- order = "c",
   stack_size = 100,
   subgroup = "elemental-baseprod"
